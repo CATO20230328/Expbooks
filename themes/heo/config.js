745c00b9993cd3405f8b91838e395295eb20ba94
CATO <129149926+CATO20230328@users.noreply.github.com>
2026-02-26 21:09:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -114,7 +114,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://www.expbook.vip',
+  HEO_SOCIAL_CARD_URL: 'https://qm.qq.com/q/L2z9cRqes2',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
