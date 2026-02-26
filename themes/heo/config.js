f1a2570fc4c42c35ecb7064c6aeb23e15192d521
CATO <129149926+CATO20230328@users.noreply.github.com>
2026-02-26 11:22:39 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,29 +8,29 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://expbooks.vercel.app' },
+    { title: '欢迎来到我的博客', url: 'https://www.expbook.vip' },
     { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
   // 博客主体区左右侧组件颠倒位置
-  HEO_HERO_BODY_REVERSE: false,
+  HEO_HERO_BODY_REVERSE: false, 
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '网站资讯',
   HEO_HERO_TITLE_2: '经验分享',
-  HEO_HERO_TITLE_3: 'expbooks.vercel.app',
-  HEO_HERO_TITLE_4: '经验分享',
-  HEO_HERO_TITLE_5: 'Notion搭建属于自己的网站（无需代码零成本）',
-  HEO_HERO_TITLE_LINK: 'https://expbooks.vercel.app',
+  HEO_HERO_TITLE_3: 'expbook.vip',
+  HEO_HERO_TITLE_4: '精选教程',
+  HEO_HERO_TITLE_5: '免费搭建属于自己的个人网站',
+  HEO_HERO_TITLE_LINK: 'https://www.expbook.vip',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_3: { title: '精选教程', url: '/tag/精选教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -39,15 +39,15 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    'EXPBOOKS'
+    'EXPBOOK'
   ],
 
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://www.expbook.vip',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://www.expbook.vip',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -114,7 +114,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://www.expbook.vip',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
