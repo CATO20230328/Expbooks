fb05f68ea3f136edbf6323056c357641431f7ebd
CATO <129149926+CATO20230328@users.noreply.github.com>
2026-04-06 23:36:16 +0800

    Update [...suffix].js

--- pages/[prefix]/[slug]/[...suffix].js
+++ pages/[prefix]/[slug]/[...suffix].js
@@ -1,42 +1,75 @@
 import BLOG from '@/blog.config'
 import { siteConfig } from '@/lib/config'
 import { fetchGlobalAllData, resolvePostProps } from '@/lib/db/SiteDataApi'
-import { checkSlugHasMorThanTwoSlash } from '@/lib/utils/post'
+import { checkSlugHasMorThanTwoSlash, processPostData } from '@/lib/utils/post'
+import { idToUuid } from 'notion-utils'
 import Slug from '..'
 
-const DynamicSlug = props => {
+/**
+ * 根据notion的slug访问页面
+ * 解析三级以上目录 /article/2023/10/29/test
+ * @param {*} props
+ * @returns
+ */
+const PrefixSlug = props => {
   return <Slug {...props} />
 }
 
+/**
+ * 编译渲染页面路径
+ * @returns
+ */
 export async function getStaticPaths() {
   if (!BLOG.isProd) {
-    return { paths: [], fallback: true }
+    return {
+      paths: [],
+      fallback: true
+    }
   }
 
   const from = 'slug-paths'
   const { allPages } = await fetchGlobalAllData({ from })
-
-  // 这里就是修复！之前你写了 ids.map，但是没定义 ids
   const paths = allPages
-    ?.filter(row => row?.slug)
+    ?.filter(row => checkSlugHasMorThanTwoSlash(row))
     .map(row => ({
       params: {
-        slug: row.slug.split('/')[0],
-        suffix: row.slug.split('/').slice(1)
+        prefix: row.slug.split('/')[0],
+        slug: row.slug.split('/')[1],
+        suffix: row.slug.split('/').slice(2)
       }
     }))
-
-  return { paths: paths || [], fallback: true }
+  return {
+    paths: paths,
+    fallback: true
+  }
 }
 
-export async function getStaticProps({ params: { slug, suffix }, locale }) {
-  const props = await resolvePostProps({ slug, suffix, locale })
+/**
+ * 抓取页面数据
+ * @param {*} param0
+ * @returns
+ */
+export async function getStaticProps({
+  params: { prefix, slug, suffix },
+  locale
+}) {
+  const props = await resolvePostProps({
+    prefix,
+    slug,
+    suffix,
+    locale,
+  })
+
   return {
     props,
     revalidate: process.env.EXPORT
       ? undefined
-      : siteConfig('NEXT_REVALIDATE_SECOND', BLOG.NEXT_REVALIDATE_SECOND, props.NOTION_CONFIG)
+      : siteConfig(
+        'NEXT_REVALIDATE_SECOND',
+        BLOG.NEXT_REVALIDATE_SECOND,
+        props.NOTION_CONFIG
+      )
   }
 }
 
-export default DynamicSlug
+export default PrefixSlug
