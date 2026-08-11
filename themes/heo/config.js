333577680e46518babc4460df5e6989a35617c28
CATO <129149926+CATO20230328@users.noreply.github.com>
2026-08-11 10:37:39 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -24,7 +24,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://www.expbooks.com' },
+    { title: 'EXPBOOK', url: 'https://www.expbooks.com' },
     { title: '免费搭建个人网站教程', url: 'https://www.expbooks.com/article/Build-a-Free-Personal-Website' }
   ],
 
